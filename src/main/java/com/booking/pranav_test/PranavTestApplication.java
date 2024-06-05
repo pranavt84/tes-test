c496224788a514562c18c8dfe3e7b4ba76674271
Pranav Tiwari <pranavt84@gmail.com>
2024-06-05 08:38:28 +0530

    First Commit after project setup

---
+++ src/main/java/com/booking/pranav_test/PranavTestApplication.java
@@ -0,0 +1,13 @@
+package com.booking.pranav_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PranavTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PranavTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/booking/pranav_test/PranavTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.booking.pranav_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PranavTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

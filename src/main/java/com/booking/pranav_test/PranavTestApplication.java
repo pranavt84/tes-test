aeaea280fd3f5da791b91e8373ee2fbed20e4d00
Pranav Tiwari <pranavt84@gmail.com>
2024-06-05 08:42:37 +0530

    Second commit

--- src/main/java/com/booking/pranav_test/PranavTestApplication.java
+++ src/main/java/com/booking/pranav_test/PranavTestApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PranavTestApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(PranavTestApplication.class, args);
 	}
 
